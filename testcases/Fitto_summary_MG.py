d6f713cbb1c9e3026c1a5c2aa90150c90b4c140f
yerin.eom <yerin.eom@olive-hc.com>
2024-02-01 15:16:46 +0900

    commit

--- testcases/Fitto_summary_MG.py
+++ testcases/Fitto_summary_MG.py
@@ -21,11 +21,22 @@ def summaryMG():
     time.sleep(1)
     el =start_fitto.driver.find_element(By.XPATH,"//android.view.ViewGroup[@resource-id='ohc.app.fitto:id/toolbar']/android.widget.FrameLayout/android.widget.LinearLayout[2]")
     if el is not None and el.is_displayed():
-        print("case 1: Summary > MQ page 진입 버튼 동작 확인\n"
-            "\033[34m" +" -----> MQ page 진입 성공" + "\033[0m")
+        print("case 1: Summary > MG page 진입 버튼 동작 확인\n"
+            "\033[34m" +" -----> MG page 진입 성공" + "\033[0m")
     else:
-        print("case 1: Summary > MQ page 진입 버튼 동작 확인\n"
-              "\033[91m" + " -----> MQ page 진입 실패" + "\033[0m")
+        print("case 1: Summary > MG page 진입 버튼 동작 확인\n"
+              "\033[91m" + " -----> MG page 진입 실패" + "\033[0m")
+
+    time.sleep(1)
+    el = start_fitto.driver.find_element(By.ID,"ohc.app.fitto:id/txtBestRecordValue")
+    if el is not None and el.is_displayed():
+        print("case 2: MG 최고 기록 순위 존재 확인\n"
+              "\033[34m" + " -----> 최고 기록 항목이 존재함" + "\033[0m")
+    else:
+        print("case 2: MG 최고 기록 순위 존재 확인\n"
+              "\033[91m" + " -----> 최고 기록 항목이 존재 하지 않음" + "\033[0m")
+
+
 
 
 
